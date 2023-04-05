e9059f5612c2af2f03ec75b90f30871da894faba
mpurdom <103977091+mpurdom@users.noreply.github.com>
2023-04-05 17:28:43 -0500

    Delete senior-project-directory1/logic directory

--- senior-project-directory1/logic/DegreePlan.py
+++
@@ -1,9 +0,0 @@
-class DegreePlan:
-    
-    def __init__(self, id, name, courses) -> None:
-        self.id = id
-        self.name = name # Computer Science
-        self.courses = courses # list of Course objects
-
-    def show_classes():
-        print("classes")
