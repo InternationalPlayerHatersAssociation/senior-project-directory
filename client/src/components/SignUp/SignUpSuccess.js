fd0219e7f78172a5416be57e1b20ebba3694a5a6
habitsxd <maxxcosmos12@gmail.com>
2023-04-24 11:15:03 -0500

    urgh

--- client/src/components/SignUp/SignUpSuccess.js
+++ client/src/components/SignUp/SignUpSuccess.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ImgWrapper } from '../InfoSection.elements'
-import successImage from '../../img/tux.PNG'
+import successImage from './Images/tux.png'
 
 const SignUpSuccess = () => {
   return (
